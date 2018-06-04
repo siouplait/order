323fdf7df45f97d749c9edd4fa38f8553b3a825e
Finskou <lelarhaz@gmail.com>
2018-06-04 15:49:41 +0200

    homepage update + css + logo

--- src/components-pages/Homepage.js
+++ src/components-pages/Homepage.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import logo from "../icon/logoo.png"
+import logo from "../components-pages/logo.png"
 import button from '../components-modules/Button.js'
 import "./Homepage.css"
 
 class Home extends Component {
   render () {
     return (
-      <div className="Home">
-      <img src={logo} className="App-logo" alt="logo" />
-    <br/>
-      <button className="s-btn s-black" >
-      Passer Commande
-      </button>
+      <div className="Home">*
+        <div class="homeLogo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      <div class="homeButton">
+        <button className="s-btn s-black" >
+        Passer Commande
+        </button>
+      </div>
+      
       </div>
     )
   }
